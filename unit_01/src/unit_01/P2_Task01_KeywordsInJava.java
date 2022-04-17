ca261289bb5f5138be94901b10d4ab918c543e32
AnupamKothari <102417770+AnupamKothari@users.noreply.github.com>
2022-04-17 09:49:29 +0530

    Keywords In Java

---
+++ unit_01/src/unit_01/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,39 @@
+package unit_01;
+
+public class P2_Task01_KeyWordsInJava {
+public static void main(String[] args)
+{
+int double=1;
+int new()
+{
+System.out.println();
+return 0;
+}
+int a=10;
+a=11;
+
+ABC int =new ABC();
+	
+}
+}
+
+class int()
+{
+void Display()
+{
+System.out.println("Another Class..1!");	
+}
+}
+
+class ABC{
+	
+	void Display()
+	{
+		System.out.println("Another Class..2!");
+		
+	}
+}
+
+public class P2_Task01_KeywordsInJava {
+
+}
